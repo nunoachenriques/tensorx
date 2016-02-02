82f33ea3e0a15a1f5d09bb99cc7a8138ba0f5822
Davide Nunes <davidelnunes@gmail.com>
2016-02-02 11:44:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,5 +33,5 @@ setup(
         'numpy >= 1.8.2',
         'tensorflow >= 0.6.0'],
 
-    dependency_links=['https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl']
+    dependency_links=['https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl']
 )
